b6b9bd6c744b7c4bd644da7534171d8ecbe4ca44
RAFAEL_ILDEFONSO_FURTADO <12402010@OMEGAB36.BARROCA>
2026-05-15 11:14:46 -0300

    aula1, aula2, aula3 feita

--- flask/aula1/aula1-OlaMundo.py
+++ flask/aula1/aula1-OlaMundo.py
@@ -11,5 +11,11 @@ def ola_mundo():
 def hello():
     return 'Hello, World!' # Isso é o que será retornado quando a rota '/hello' for acessada
 
+@app.route('/decorator')
+def decorator():
+    return ("""<b>O que é um decorator em Python:</b> Um decorator em Python é uma ferramenta poderosa que permite modificar ou estender o comportamento de funções ou classes sem alterar permanentemente o código original delas.<br><br>
+            <b>Para que ele serve:</b> Basicamente, o decorator serve para evitar repetição de código (DRY - Don't Repeat Yourself) e manter sua lógica principal limpa. Ele "sequestra" a execução de uma função para injetar comportamentos extras.<br><br>
+            <b>Como ele é utilizado no Flask (exemplo: @app.route):</b> Na Criação de Rotas, Proteção de Acesso, Tratamento de Erros, Ciclo de Vida da Requisição""")
+
 if __name__ == '__main__':
     app.run(debug=True) # Isso inicia o servidor Flask em modo de depuração, o que é útil para desenvolvimento
