0a7d56b4dddaf7443edf8ddadc6771d4f064d0a6
Rafael Ildefonso <rafaelfurtado313@gmail.com>
2026-05-22 21:38:05 -0300

    atividade 4 e 5 feita

--- flask/Aula5/app.py
+++ flask/Aula5/app.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route('/page1')
 def page1():
-    return render_template('page1.html')    
+    return render_template('page1.html')
 
 @app.route('/page2')
 def page2():    
--- flask/Aula6/Aula6.py
+++ flask/Aula6/Aula6.py
@@ -2,10 +2,26 @@ from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
+usuarios = [
+    {'usuario': 'rafael', 'senha': '12402010'},
+    {'usuario': 'marcos', 'senha': 'cotemig2026'},
+    {'usuario': 'janaina', 'senha': 'cotemig2026'}
+]
+
+def verificar_credenciais(usuario, senha):
+    """
+    Função que PERCORRE o dicionário com FOR
+    Itera sobre a lista de dicionários 'usuarios'
+    """
+    for u in usuarios:
+        if u['usuario'] == usuario and u['senha'] == senha:
+            return True
+    return False
+
 def show_the_login_form():
     return render_template_string("""
         <h2>Login</h2>
-        <form method="POST">
+        <form action="/login" method="POST">
             <input type="text" name="usuario" placeholder="Usuário"><br><br>
             <input type="password" name="senha" placeholder="Senha"><br><br>
             <button type="submit">Entrar</button>
@@ -16,10 +32,10 @@ def do_the_login():
     usuario = request.form.get('usuario')
     senha = request.form.get('senha')
 
-    if usuario == 'admin' and senha == '123':
-        return f"<h1>Bem-vindo, {usuario}!</h1>"
+    if verificar_credenciais(usuario, senha):
+        return f"<h1>Bem-vindo, {usuario}!</h1><a href='/login'>Voltar</a>"
     else:
-        return "<h1>Login inválido</h1>"
+        return "<h1>Login inválido</h1><a href='/login'>Tentar novamente</a>"
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,7 +44,9 @@ def login():
     else:
         return show_the_login_form()
 
+@app.route('/')
+def index():
+    return login()
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-# site de consulta https://flask.palletsprojects.com/en/stable/quickstart/#html-escaping
